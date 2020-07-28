4e1d9bac48e34b3dac3298c618360bcfac698e0f
Alexey <aogloblin278@gmail.com>
2020-07-28 19:07:14 +0300

    home page and PostDetail

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from blog.models import Category, Post, Product
+from user.models import Profile
+
+
+class HomePost(ListView):
+    model = Post
+    template_name = 'home.html'
+    context_object_name = 'home_posts'
+    queryset = Post.objects.filter(to_display=True)
+
+    # def get_queryset(self):
+    #     Post.objects.filter(to_display=True)
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+    slug_field = 'post_id'
+
+
+# class CategoryList(ListView):
+#     model = Category
+#     template_name = 'base.html'
+
+
+# class CategoryDetail(DetailView):
+#     model = Post
+#     template_name = 'category_detail.html'
+#
+#     def get_queryset(self):
+#         Post.objects.filter(category__slug=self.kwargs['category_slug'])
+
+
+
+class ProductByPost(ListView):
+    model = Product
+    template_name = 'post_detail.html'
+
+    def get_queryset(self):
+        Product.objects.filter(to_display=True, post__slug=self.kwargs['post_detail'])
